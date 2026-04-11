037fd7cb045647e6474f362c0017578145aac688
wjl <wjl@bupt.edu.cn>
2026-04-11 21:09:26 +0800

    [dae][approx_adv]: reback version

--- con_algo/dae/ppo.py
+++ con_algo/dae/ppo.py
@@ -879,7 +879,6 @@ class CustomPPO(OnPolicyAlgorithm):
                     # values,
                     last_values,
                     beta = huber_loss_beta,
-                    use_lambda = True
                 )
                 main_value_loss_2, beta = self._value_loss(
                     rewards.split(lengths), 
@@ -890,7 +889,7 @@ class CustomPPO(OnPolicyAlgorithm):
                     beta = huber_loss_beta,
                     use_lambda=True,
                 )
-                main_value_loss = main_value_loss_1 + main_value_loss_2
+                main_value_loss = 0.5 * (main_value_loss_1 + main_value_loss_2)
                 value_loss = self.vf_coef * main_value_loss + cur_corr_coef * (1 - corr)
                 # value_loss = self.vf_coef * value_loss + 0.1 * (1.0 / (advantages.std() + 1.0)).mean() 
                 # value_loss += (ex_adv**2).mean()
@@ -1004,7 +1003,7 @@ class CustomPPO(OnPolicyAlgorithm):
             batch_size = self.batch_size, 
             gamma = self.gamma, 
             gae_like_lambda = self.gae_like_lambda,
-            use_gae_like = False,
+            use_gae_like = True,
         )
         
         # if self.advantage_normalization:
